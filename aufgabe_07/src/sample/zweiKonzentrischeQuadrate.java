0adefbd1a1242b8c2c8d442bf7d645f6ee66699b
Sebastian Schramm <s-schramm1@gmx.de>
2021-02-16 22:03:25 +0100

    update apa notation

--- aufgabe_07/src/sample/zweiKonzentrischeQuadrate.java
+++ aufgabe_07/src/sample/zweiKonzentrischeQuadrate.java
@@ -3,6 +3,7 @@ package sample;
 public class zweiKonzentrischeQuadrate {
 	private Draw draw;
 	private int length = 200;
+	private int anzahlQuadrate = 2;
 
 	public zweiKonzentrischeQuadrate(Draw draw) {
 		this.draw = draw;
@@ -11,11 +12,11 @@ public class zweiKonzentrischeQuadrate {
 	}
 	
 	private void print2() {
-		for (int i = 0; i < 2; ++i) {
-			draw.turn(90);
-			draw.move(length / 2);
-			draw.turn(-90);
-			draw.move(length / 2);
+		for (int i = 0; i < anzahlQuadrate; ++i) {
+			for (int n = 0; n < 2; ++n) {
+				draw.turn((n % 2) != 0 ? -90 : 90);
+				draw.move(length / 2);
+			}
 			draw.down();
 			for (int n = 0; n < 4; ++n) {
 				draw.move(length);
